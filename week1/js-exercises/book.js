b113e6ffaa53216db53d10032f3f16979aef0482
Mimi-codes <65224611+Mimi-codes@users.noreply.github.com>
2021-02-04 11:14:17 +0100

    Update book.js

--- week1/js-exercises/book.js
+++ week1/js-exercises/book.js
@@ -1,72 +1,93 @@
-const books = [
+const books = [ //array of books
     {
       title: 'The Design of Everyday Things',
       author: 'Don Norman',
       alreadyRead: false,
-    URL: 'https://img1.od-cdn.com/ImageType-400/4575-1/85E/A00/CF/%7B85EA00CF-F096-4068-8876-960C1075ED20%7DImg400.jpg' },
+    },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       alreadyRead: true,
-      URL: 'https://images1.penguinrandomhouse.com/cover/9780307476708',
-    
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       alreadyRead: true,
-      URL: 'https://res.cloudinary.com/practicaldev/image/fetch/s--l6RWhB1I--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://upload.wikimedia.org/wikipedia/en/8/8f/The_pragmatic_programmer.jpg',
-   
     }
   ];
 
-  const newDiv = document.querySelector('#body')
+  document.querySelector('title').textContent = 'Book List'; //assign 'Book List' to title
 
-  //loops through the array of books.
-  const newUl = document.createElement('ul');
-  books.forEach(book => {
-  let listItem = document.createElement('li');
-  listItem.style.display = 'inline-block';
-  listItem.style.width = '50%';
-  listItem.style.margin = '3%';
-  listItem.style.paddingRight = '20%'
-  listItem.style.paddingBottom = '7%'
-  listItem.style.textAlign = 'center';
-  let image = document.createElement('img');
-  image.src = book.URL;
-  image.style.width = '90%';
-  image.style.height = '100%';
-  let newParagraph = document.createElement('p');
-  newParagraph.innerText = `${book.title} - ${book.author}`;
-  newParagraph.style.color = 'black';
-  newParagraph.style.paddingLeft = '1%';
-  listItem.appendChild(newParagraph);
-  listItem.appendChild(image);
-  newUl.appendChild(listItem);
+
+  let design = document.getElementById('norman');
+  design.innerHTML = '<p>The Design of Everyday Things - Don Norman</p>'; //add 'p' tag to the div
+ 
+  let newImage = document.createElement("img");
+newImage.src = 'https://img1.od-cdn.com/ImageType-400/4575-1/85E/A00/CF/%7B85EA00CF-F096-4068-8876-960C1075ED20%7DImg400.jpg';
+design.appendChild(newImage); //appends image to design 
+
+
+  let human = document.getElementById('christian');
+  human.innerHTML = '<p>The Most Human Human - Brian Christian</p>'; //add 'p' tag to the div
   
-  if(book.alreadyRead === true) {
-    listItem.style.backgroundColor = 'green';
-  } else {
-    listItem.style.backgroundColor = 'red';
-  }
-  
-   newDiv.appendChild(newUl);
-   newDiv.style.textAlign = 'center';
- });
-  
+  let newImage2 = document.createElement("img");
+newImage2.src = 'https://images1.penguinrandomhouse.com/cover/9780307476708';
+ human.appendChild(newImage2); //appends image to human
 
+  let pragmatic = document.getElementById('hunt');
+  pragmatic.innerHTML = '<p class="hunt">The Pragmatic Programmer - Andrew Hunt</p>'; //add 'p' tag to the div
 
-/*books.forEach(function (arrayItem) {
-  var x = arrayItem.prop1 + 2;
-  console.log(x);
-});
-  */
-
-/*for(var i = 0; i < books.length; i++) {
-  var newParagraph = document.createElement('p');
-  var newParagraphText = document.createTextNode('Norman');
-  newParagraph.appendChild(newParagraphText);
-  //textContent = `${title}` + `${author}`;
+  let newImage3 = document.createElement("img");
+  newImage3.src =   'https://res.cloudinary.com/practicaldev/image/fetch/s--l6RWhB1I--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://upload.wikimedia.org/wikipedia/en/8/8f/The_pragmatic_programmer.jpg';
+   pragmatic.appendChild(newImage3); //appends image to pragmatic
    
+
+   let mainDiv = document.getElementById('main'); //call and assign the id of main to 'mainDiv'
+
+    //styling
+mainDiv.style = 'display: flex'; 
+document.getElementById('norman').style.marginLeft = '2%';
+document.getElementById('norman').style.width = '30%';
+
+document.getElementById('christian').style.marginLeft = '2%';
+document.getElementById('christian').style.width = '30%';
+
+document.getElementById('hunt').style.marginLeft = '2%';
+document.getElementById('hunt').style.marginRight = '2%';
+document.getElementById('hunt').style.width = '30%';
+
+newImage.style.paddingLeft = '4%';
+newImage.style.height = '85%';
+newImage.style.width = '63%';
+
+newImage2.style.paddingLeft = '4%';
+newImage2.style.height = '85%';
+newImage2.style.width = '63%';
+
+newImage3.style.paddingLeft = '4%';
+newImage3.style.height = '85%';
+newImage3.style.width = '63%';
+
+human.style.paddingLeft = '2%';
+pragmatic.style.paddingLeft = '2%';
+design.style.paddingLeft = '2%';
+
+//conditional statements
+if(books.alreadyRead === true) {
+  norman.style.backgroundColor = 'green';
+} else {
+  norman.style.backgroundColor = 'red';
+}  
+
+if(books.alreadyRead === true) {
+  christian.style.backgroundColor = 'red';
 }
-*/
+ else {
+christian.style.backgroundColor = 'green';
+}  
+
+if(books.alreadyRead === true) { 
+ hunt.style.backgroundColor = 'red';
+} else {
+  hunt.style.backgroundColor = 'green';
+}; 
